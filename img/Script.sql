56c81a76debdd581771876b258ae68705f56fad2
valery-dubinin <153645498+valery-dubinin@users.noreply.github.com>
2024-03-25 15:53:49 +0300

    Add files via upload

---
+++ img/Script.sql
@@ -0,0 +1,20 @@
+USE sakila;
+
+select DISTINCT district
+from address a 
+where district like 'K%a' and district not like '% %';
+
+select *
+from payment p 
+where amount > 10 and date(payment_date) between '2005-06-15' and '2005-06-18'
+order by payment_date, amount ;
+
+select *
+from rental r 
+where 
+order by rental_date desc
+limit 5;
+
+select replace(lower(first_name), 'll', 'pp'), replace(lower(last_name), 'll', 'pp'), email 
+from customer c 
+where first_name = 'KELLY' or first_name = 'WILLIE';
